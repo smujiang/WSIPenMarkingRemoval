ef1ee6d0246e58f02d7291e2617363c089b5558a
Jun Jiang <smujiang@gmail.com>
2020-07-16 16:38:28 -0500

    update evaluation scripts

--- pix2pix/training_cv_0.sh
+++ pix2pix/training_cv_0.sh
@@ -4,8 +4,8 @@ export CUDA_VISIBLE_DEVICES=0
 patch_root_dir=/projects/shart/digital_pathology/data/PenMarking/WSIs_Img_pairs_256   # directory where saves image patch pairs for training
 
 # train with 50 cases
-training_case_txt_50=/projects/shart/digital_pathology/data/PenMarking/WSIs/training_cases_50_cv_5.txt # directory where saves WSI training cases
-Train_output=/projects/shart/digital_pathology/data/PenMarking/model/pixel2pixel_256_50cases_cv5  # Output directory to save trained models.
+training_case_txt_50=/projects/shart/digital_pathology/data/PenMarking/WSIs/training_cases_50_cv_0.txt # directory where saves WSI training cases
+Train_output=/projects/shart/digital_pathology/data/PenMarking/model/pixel2pixel_256_50cases_cv0  # Output directory to save trained models.
 
 python pix2pix.py \
   --mode train \
