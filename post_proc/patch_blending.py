e74eee2dc498eb65979986c9c499354bf0f11ca2
Jun Jiang <smujiang@gmail.com>
2020-07-16 21:00:30 -0500

    update

--- post_proc/patch_blending.py
+++ post_proc/patch_blending.py
@@ -271,43 +271,43 @@ if __name__ == "__main__":
     wsi_uuid_list = ["7470963d479b4576bc8768b389b1882e", "4e5a6beed06d4ce48be735e1f3c3abc1",
                      "d83cc7d1c941438e93786fc381ab5bb5", "a0e53609686a4ae9a824d9525641dc56",
                      "c477c949f26a40eca92657b1bcf5dcca"]
-    org_location_1 = [[40800, 39824], [37908, 41216], [41908, 41216], [38908, 40216], [47071, 37706]]
+    org_location_1 = [[39824, 40800], [41216, 37908], [41216, 41908], [40216, 38908], [47071, 37706]]
     org_location_2 = [[40872, 27687], [42764, 24895], [44208, 26498], [47548, 11087], [45721, 13120]]
     org_location_3 = [[21723, 40008], [26019, 26304], [34843, 28973], [68357, 54662], [76221, 58489]]
-    org_location_4 = [[29220, 49680], [59996, 37196], [61779, 36414], [40985, 59544], [50038, 29307]]
+    org_location_4 = [[29220, 49680], [59996, 37196], [61779, 36414], [17423, 39241], [50038, 29307]]
     org_location_5 = [[37328, 41051], [21070, 33279], [31096, 24155], [68732, 41287], [38525, 33302]]
     org_location_list = [org_location_1, org_location_2, org_location_3, org_location_4, org_location_5]
     img_dir = "/projects/shart/digital_pathology/data/PenMarking/eval/pixel2pixel_256/images_dispatch"
     img_dir_out = "/projects/shart/digital_pathology/data/PenMarking/eval/pixel2pixel_256/patch_blendings"
 
-    for wsi_uuid in wsi_uuid_list:
+    for idx, wsi_uuid in enumerate(wsi_uuid_list):
         print("processing case: %s" % wsi_uuid)
         case_dir = os.path.join(img_dir, wsi_uuid)
-        for case_org_locations in org_location_list:
-            for temp in case_org_locations:
-                org_location = [temp[1], temp[0]]
-                # blended_rec_img, direct_rec_img = restore_region(org_location, region_size, step_sz, patch_sz, channels, img_dir, chop=True)
-                blended_rec_img, direct_rec_img, original_img, target_img = restore_region(org_location, region_size, step_sz, patch_sz, channels, case_dir, wsi_uuid, chop=False)
-                # print(blended_rec_img.shape)
-                # print(direct_rec_img.shape)
-                # fig, (axs1, axs2) = plt.subplots(1, 2)
-                # axs1.imshow(direct_rec_img)
-                # axs2.imshow(blended_rec_img)
-                # axs1.axis("off")
-                # axs2.axis("off")
-                # plt.show()
+        for org_location in org_location_list[idx]:
 
-                if not os.path.exists(os.path.join(img_dir_out, wsi_uuid)):
-                    os.makedirs(os.path.join(img_dir_out, wsi_uuid))
+            print("Restore region %d, %d" % (org_location[0], org_location[1]))
+            # blended_rec_img, direct_rec_img = restore_region(org_location, region_size, step_sz, patch_sz, channels, img_dir, chop=True)
+            blended_rec_img, direct_rec_img, original_img, target_img = restore_region(org_location, region_size, step_sz, patch_sz, channels, case_dir, wsi_uuid, chop=False)
+            # print(blended_rec_img.shape)
+            # print(direct_rec_img.shape)
+            # fig, (axs1, axs2) = plt.subplots(1, 2)
+            # axs1.imshow(direct_rec_img)
+            # axs2.imshow(blended_rec_img)
+            # axs1.axis("off")
+            # axs2.axis("off")
+            # plt.show()
 
-                direct_name = os.path.join(img_dir_out, wsi_uuid, "direct_stitch"+str(org_location)+".jpg")
-                Image.fromarray(direct_rec_img).save(direct_name)
-                blending_name = os.path.join(img_dir_out, wsi_uuid, "blending_stitch"+str(org_location)+".jpg")
-                Image.fromarray(blended_rec_img).save(blending_name)
-                org_name = os.path.join(img_dir_out, wsi_uuid, "original_img"+str(org_location)+".jpg")
-                Image.fromarray(original_img).save(org_name)
-                tar_name = os.path.join(img_dir_out, wsi_uuid, "target_img" + str(org_location) + ".jpg")
-                Image.fromarray(target_img).save(tar_name)
+            if not os.path.exists(os.path.join(img_dir_out, wsi_uuid)):
+                os.makedirs(os.path.join(img_dir_out, wsi_uuid))
+
+            direct_name = os.path.join(img_dir_out, wsi_uuid, "direct_stitch"+str(org_location)+".jpg")
+            Image.fromarray(direct_rec_img).save(direct_name)
+            blending_name = os.path.join(img_dir_out, wsi_uuid, "blending_stitch"+str(org_location)+".jpg")
+            Image.fromarray(blended_rec_img).save(blending_name)
+            org_name = os.path.join(img_dir_out, wsi_uuid, "original_img"+str(org_location)+".jpg")
+            Image.fromarray(original_img).save(org_name)
+            tar_name = os.path.join(img_dir_out, wsi_uuid, "target_img" + str(org_location) + ".jpg")
+            Image.fromarray(target_img).save(tar_name)
 
 
 
