d43bd1b36aa45d65ffb37fa7a12933ec514fd03f
Jun Jiang <smujiang@gmail.com>
2020-07-16 21:21:00 -0500

    Update evaluation.py

--- post_proc/evaluation.py
+++ post_proc/evaluation.py
@@ -17,25 +17,28 @@ org_location_list = [org_location_1, org_location_2, org_location_3, org_locatio
 img_dir = "/projects/shart/digital_pathology/data/PenMarking/eval/pixel2pixel_256/images_dispatch"
 img_dir_out = "/projects/shart/digital_pathology/data/PenMarking/eval/pixel2pixel_256/patch_blendings"
 
-for wsi_uuid in wsi_uuid_list:
+str_wrt = "PSNR, SSIM, VIF, PSNR_blended, SSIM_blended, VIF_blended, PSNR_stitch, SSIM_stitch, VIF_stitch\n"
+fp = open(os.path.join(img_dir_out, "eval_result.csv"), 'w')
+for idx, wsi_uuid in enumerate(wsi_uuid_list):
     print("processing case: %s" % wsi_uuid)
     case_dir = os.path.join(img_dir, wsi_uuid)
-    for case_org_locations in org_location_list:
-        for org_location in case_org_locations:
-            direct_name = os.path.join(img_dir_out, wsi_uuid, "direct_stitch" + str(org_location) + ".jpg")
-            direct_rec_img = Image.open(direct_name)
-            blending_name = os.path.join(img_dir_out, wsi_uuid, "blending_stitch" + str(org_location) + ".jpg")
-            blended_rec_img = Image.open(blending_name)
-            org_name = os.path.join(img_dir_out, wsi_uuid, "original_img" + str(org_location) + ".jpg")
-            original_img = Image.open(org_name)
-            tar_name = os.path.join(img_dir_out, wsi_uuid, "target_img" + str(org_location) + ".jpg")
-            target_img = Image.open(tar_name)
+    for org_location in org_location_list[idx]:
+        direct_name = os.path.join(img_dir_out, wsi_uuid, "direct_stitch" + str(org_location) + ".jpg")
+        direct_rec_img = Image.open(direct_name)
+        blending_name = os.path.join(img_dir_out, wsi_uuid, "blending_stitch" + str(org_location) + ".jpg")
+        blended_rec_img = Image.open(blending_name)
+        org_name = os.path.join(img_dir_out, wsi_uuid, "original_img" + str(org_location) + ".jpg")
+        original_img = Image.open(org_name)
+        tar_name = os.path.join(img_dir_out, wsi_uuid, "target_img" + str(org_location) + ".jpg")
+        target_img = Image.open(tar_name)
 
-            PSNR, SSIM, VIF = calculate_metrics(np.array(target_img), np.array(original_img))
-            PSNR_b, SSIM_b, VIF_b = calculate_metrics(np.array(target_img), np.array(blended_rec_img))
-            PSNR_s, SSIM_s, VIF_s = calculate_metrics(np.array(target_img), np.array(direct_rec_img))
+        PSNR, SSIM, VIF = calculate_metrics(np.array(target_img), np.array(original_img))
+        PSNR_b, SSIM_b, VIF_b = calculate_metrics(np.array(target_img), np.array(blended_rec_img))
+        PSNR_s, SSIM_s, VIF_s = calculate_metrics(np.array(target_img), np.array(direct_rec_img))
 
-            print("%f, %f, %f" %(PSNR, SSIM, VIF))
-            print("%f, %f, %f" %(PSNR_b, SSIM_b, VIF_b))
-            print("%f, %f, %f" %(PSNR_s, SSIM_s, VIF_s))
+        s = "%f,%f,%f,%f,%f,%f,%f,%f,%f\n" %(PSNR, SSIM, VIF, PSNR_b, SSIM_b, VIF_b, PSNR_s, SSIM_s, VIF_s)
+        str_wrt += s
+
+fp.write(str_wrt)
+fp.close()
 
